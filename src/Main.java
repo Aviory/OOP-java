53e060243bfe7720baa8e7902b1ee5173c719e64
Vel <avior@gmail.com>
2024-11-27 20:37:14 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        String s = "Hello";
+        String s1 = "Hello";
+        String s3 = "Hello";
+        //String immutable;
+        //String immutable;
+
+
+        //work(s1);
+
+
+        System.out.println(s.equals(s1));
+    }
+
+    //Написати метод, який підраховує кількість голосних літер(a, e, i, o, u) у заданому рядку.
+
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/Aviory/task4.git
+//    git push -u origin main
+
+
+}
